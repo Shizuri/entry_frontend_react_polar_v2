45160eb6fbf22199e92e892374c2aa3b5e0f6658
Zdravko Mavkov <mavkov@hotmail.com>
2020-10-11 14:23:50 +0200

    Implemented the new select menus. Do final touches on mobile item placement.

--- src/CardsPage.js
+++ src/CardsPage.js
@@ -5,17 +5,50 @@ import { Context } from './context'
 import { Link } from 'react-router-dom'
 import Card from './Card'
 
+import MultiSelect from 'react-multi-select-component'
+
 // JSON extracted from the MtG API for faster testing
 // Comment it in with it's useEffect call
-// import cardsJson from './cards.json'
+import cardsJson from './cards.json'
 
 // Constant values that are frozen so that they can't be mutated in any way
-const ALL_CARD_TYPES = ['Artifact', 'Autobot', 'Card', 'Character', 'Conspiracy', 'Creature', 'Dragon', 'Elemental',
-    'Enchantment', 'Goblin', 'Hero', 'Instant', 'Jaguar', 'Knights', 'Land', 'Phenomenon', 'Plane', 'Planeswalker',
-    'Scheme', 'Sorcery', 'Specter', 'Summon', 'Tribal', 'Vanguard', 'Wolf', 'You’ll']
-Object.freeze(ALL_CARD_TYPES)
-const ALL_CARD_COLORS = ['White', 'Blue', 'Black', 'Red', 'Green']
-Object.freeze(ALL_CARD_COLORS)
+const ALL_CARD_TYPES_OBJECTS = [
+    { label: 'Artifact', value: 'Artifact' },
+    { label: 'Autobot', value: 'Autobot' },
+    { label: 'Card', value: 'Card' },
+    { label: 'Character', value: 'Character' },
+    { label: 'Conspiracy', value: 'Conspiracy' },
+    { label: 'Creature', value: 'Creature' },
+    { label: 'Dragon', value: 'Dragon' },
+    { label: 'Elemental', value: 'Elemental' },
+    { label: 'Enchantment', value: 'Enchantment' },
+    { label: 'Goblin', value: 'Goblin' },
+    { label: 'Hero', value: 'Hero' },
+    { label: 'Instant', value: 'Instant' },
+    { label: 'Jaguar', value: 'Jaguar' },
+    { label: 'Knights', value: 'Knights' },
+    { label: 'Land', value: 'Land' },
+    { label: 'Phenomenon', value: 'Phenomenon' },
+    { label: 'Plane', value: 'Plane' },
+    { label: 'Planeswalker', value: 'Planeswalker' },
+    { label: 'Scheme', value: 'Scheme' },
+    { label: 'Sorcery', value: 'Sorcery' },
+    { label: 'Specter', value: 'Specter' },
+    { label: 'Summon', value: 'Summon' },
+    { label: 'Tribal', value: 'Tribal' },
+    { label: 'Vanguard', value: 'Vanguard' },
+    { label: 'Wolf', value: 'Wolf' },
+    { label: 'You’ll', value: 'You’ll' }
+]
+Object.freeze(ALL_CARD_TYPES_OBJECTS)
+const ALL_CARD_COLORS_OBJECTS = [
+    { label: 'White', value: 'White' },
+    { label: 'Blue', value: 'Blue' },
+    { label: 'Black', value: 'Black' },
+    { label: 'Red', value: 'Red' },
+    { label: 'Green', value: 'Green' }
+]
+Object.freeze(ALL_CARD_COLORS_OBJECTS)
 
 const CardsPage = props => {
     // The global state
@@ -25,30 +58,12 @@ const CardsPage = props => {
     const [filteredCards, setFilteredCards] = useState([]) // Filtered cards
 
     const [searchTerm, setSearchTerm] = useState('') // Search term to filter cards by
-    const [cardTypeSelectedValues, setCardTypeSelectedValues] = useState([]) // Card type value
     const [cardColorSelectedValues, setCardColorSelectedValues] = useState([]) // Card color value
+    const [cardTypeSelectedValues, setCardTypeSelectedValues] = useState([]) // Card type value
     const [cardOrder, setCardOrder] = useState('Sort')
 
     const handleSearchTerm = event => {
         setSearchTerm(event.target.value)
-    }
-
-    const handleTypeChange = event => {
-        const selected = []
-        let selectedOption = (event.target.selectedOptions)
-        for (let i = 0; i < selectedOption.length; i++) {
-            selected.push(selectedOption.item(i).value)
-        }
-        setCardTypeSelectedValues(selected)
-    }
-
-    const handleColorChange = event => {
-        const selected = []
-        let selectedOption = (event.target.selectedOptions)
-        for (let i = 0; i < selectedOption.length; i++) {
-            selected.push(selectedOption.item(i).value)
-        }
-        setCardColorSelectedValues(selected)
     }
 
     const handleSortingOrder = event => {
@@ -67,48 +82,48 @@ const CardsPage = props => {
     }
 
     // Load cards
-    useEffect(() => {
-        let isMounted = true
+    // useEffect(() => {
+    //     let isMounted = true
 
-        const getCards = () => {
-            // GET Request.
-            fetch('https://api.magicthegathering.io/v1/cards?random=true&pageSize=100&language=English/')
-                // Handle success
-                // Convert to json
-                .then(response => {
-                    if (isMounted) {
-                        return response.json()
-                    }
-                })
-                .then(data => {
-                    if (isMounted) {
-                        setCards(data.cards) // Set global state cards
-                        setFilteredCards(data.cards) // Set filtered cards that will be displayed
-                        setLoaded(true) // Cards are loaded
-                    }
-                })
-                // Catch errors
-                .catch(error => {
-                    if (isMounted) {
-                        console.log('Request Failed with error: ', error)
-                    }
-                })
+    //     const getCards = () => {
+    //         // GET Request.
+    //         fetch('https://api.magicthegathering.io/v1/cards?random=true&pageSize=100&language=English/')
+    //             // Handle success
+    //             // Convert to json
+    //             .then(response => {
+    //                 if (isMounted) {
+    //                     return response.json()
+    //                 }
+    //             })
+    //             .then(data => {
+    //                 if (isMounted) {
+    //                     setCards(data.cards) // Set global state cards
+    //                     setFilteredCards(data.cards) // Set filtered cards that will be displayed
+    //                     setLoaded(true) // Cards are loaded
+    //                 }
+    //             })
+    //             // Catch errors
+    //             .catch(error => {
+    //                 if (isMounted) {
+    //                     console.log('Request Failed with error: ', error)
+    //                 }
+    //             })
 
-        }
+    //     }
 
-        getCards()
-        // Set isMounted to false so that the fetch request does not happen
-        // This prevents a memory leak scenario on a fast component unmount
-        return () => { isMounted = false }
-    }, [setCards])
+    //     getCards()
+    //     // Set isMounted to false so that the fetch request does not happen
+    //     // This prevents a memory leak scenario on a fast component unmount
+    //     return () => { isMounted = false }
+    // }, [setCards])
 
     // Load cards dummy effect with local JSON
     // Comment the previous useEffect and comment this out for faster testing
-    // useEffect(() => {
-    //     setCards(cardsJson.cards)
-    //     setFilteredCards(cardsJson.cards)
-    //     setLoaded(true)
-    // }, [setCards])
+    useEffect(() => {
+        setCards(cardsJson.cards)
+        setFilteredCards(cardsJson.cards)
+        setLoaded(true)
+    }, [setCards])
 
     // If the name was reset, usually because of a page reload, read it from localStorage
     useEffect(() => {
@@ -146,7 +161,7 @@ const CardsPage = props => {
                     for (let j = 0; j < cardTypeSelectedValues.length; j++) {
                         // If any member of the card types array is found in the array of selected types, filter it in
                         // This will make sure that if the card has more than one type it will still be shown
-                        if (card.types[i].toLowerCase().includes(cardTypeSelectedValues[j].toLowerCase())) {
+                        if (card.types[i].toLowerCase().includes(cardTypeSelectedValues[j].value.toLowerCase())) {
                             return true
                         }
                     }
@@ -165,7 +180,7 @@ const CardsPage = props => {
             if (card.colors) {
                 for (let i = 0; i < card.colors.length; i++) {
                     for (let j = 0; j < cardColorSelectedValues.length; j++) {
-                        if (card.colors[i].toLowerCase().includes(cardColorSelectedValues[j].toLowerCase())) {
+                        if (card.colors[i].toLowerCase().includes(cardColorSelectedValues[j].value.toLowerCase())) {
                             // If any member of the card types array is found in the array of selected types, filter it in
                             // This will make sure that if the card has more than one type it will still be shown
                             return true
@@ -189,33 +204,44 @@ const CardsPage = props => {
             <div className='CardsPage-controls'>
                 <div className='CardsPage-controls-left-or-top-panel'>
                     <label htmlFor='CardsPage-card-color-selector'>Filter cards by <strong>color</strong></label>
-                    <select className='CardsPage-card-color-selector' id='CardsPage-card-color-selector' multiple={true} value={cardColorSelectedValues} onChange={handleColorChange}>
-                        {ALL_CARD_COLORS.map(item => <option className='CardsPage-card-color-option' key={item} value={item}>{item}</option>)}
-                    </select>
+                    <MultiSelect
+                        className='CardsPage-card-color-selector'
+                        options={ALL_CARD_COLORS_OBJECTS}
+                        value={cardColorSelectedValues}
+                        onChange={setCardColorSelectedValues}
+                        // shouldToggleOnHover={true}
+                        focusSearchOnOpen={false}
+                    />
 
                     <label htmlFor='CardsPage-card-type-selector'>Filter cards by <strong>type</strong></label>
-                    <select className='CardsPage-card-type-selector' id='CardsPage-card-type-selector' multiple={true} value={cardTypeSelectedValues} onChange={handleTypeChange}>
-                        {ALL_CARD_TYPES.map(item => <option className='CardsPage-card-type-option' key={item} value={item}>{item}</option>)}
-                    </select>
+                    <MultiSelect
+                        className='CardsPage-card-type-selector'
+                        options={ALL_CARD_TYPES_OBJECTS}
+                        value={cardTypeSelectedValues}
+                        onChange={setCardTypeSelectedValues}
+                        // shouldToggleOnHover={true}
+                        focusSearchOnOpen={false}
+                    />
+
+                    <div className='CardsPage-number-of-cards-found'>Cards found: <strong>{filteredCards.length}</strong></div>
                 </div>
 
                 <div className='CardsPage-controls-right-or-bottom-panel'>
-                    <label htmlFor='CardsPage-search-term-filter'>Filter cards by name or text</label>
+                    <label htmlFor='CardsPage-search-term-filter'>Filter cards by <strong>name</strong> or <strong>text</strong></label>
                     <input type='text' className='CardsPage-search-term-filter' name='CardsPage-search-term-filter' id='CardsPage-search-term-filter' value={searchTerm} onChange={handleSearchTerm} />
 
-                    <label htmlFor='CardsPage-alphabetic-sorting-selector'>Sort card alphabetically</label>
+                    <label htmlFor='CardsPage-alphabetic-sorting-selector'>Sort card <strong>alphabetically</strong></label>
                     <select className='CardsPage-alphabetic-sorting-selector' name='CardsPage-alphabetic-sorting-selector' id='CardsPage-alphabetic-sorting-selector' value={cardOrder} onChange={handleSortingOrder}>
                         <option className='CardsPage-sorting-option' value='Sort' disabled defaultValue>Sort</option>
                         <option className='CardsPage-sorting-option' value='Ascending'>Ascending</option>
                         <option className='CardsPage-sorting-option' value='Descending'>Descending</option>
                     </select>
-
-                    <div className='CardsPage-number-of-cards-found'>Cards found: <strong>{filteredCards.length}</strong></div>
-
                     <Link className='CardsPage-back-button' to='/'>Home</Link>
+
+
                 </div>
             </div>
-            <div className='CardPage-explanation'><em>Please hold the CTRL button when clicking to deselect or to select multiple options</em></div>
+
 
             <div className='CardsPage-cards'>
                 {loaded ? filteredCards.map(card =>
